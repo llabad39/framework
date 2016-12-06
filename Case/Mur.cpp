4fdbb36a8dcf53e93e353fa6fd5226ff5d253590
llabad39 <lucaslabadens@hotmail.fr>
2016-12-06 12:47:33 +0100

    sokoban

--- Case/Mur.cpp
+++ Case/Mur.cpp
@@ -5,11 +5,8 @@ bool Mur::isEmpty(){
 }
 
 bool Mur::moveTo(shared_ptr<Case> c){
-<<<<<<< HEAD
   return false;
-=======
-    return false;
->>>>>>> 0b1c049a6a024febcc990996d5b4884e456de8ed
+
 }
 void Mur::affiche(){
   cout << "M";
--- Case/Vide.cpp
+++ Case/Vide.cpp
@@ -8,8 +8,5 @@ bool Vide::moveTo(shared_ptr<Case> c){
   return true;
 }
 
-<<<<<<< HEAD
 void Vide::affiche(){ cout << " "; }
 void Vide::endTurn(){};
-=======
->>>>>>> 0b1c049a6a024febcc990996d5b4884e456de8ed
