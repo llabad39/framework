ba88665ca4dcd60559a1354b02f2faf821009e81
llabad39 <lucaslabadens@hotmail.fr>
2016-12-14 16:23:51 +0100

    main

--- Case/Main.cpp
+++ Case/Main.cpp
@@ -1,4 +1,5 @@
 #include "Jeu2048.hpp"
+#include "JeuTaquin.hpp"
 #include <iostream>
 #include <SFML/Graphics.hpp>
 int main(){
@@ -6,7 +7,21 @@ int main(){
   int taille;
   int jeu;
   
-  cout << 
-  cin << taille;
+  cout << "entrez votre type de jeu\n";
+  cin >> jeu;
+  cout << "entrez la taille du plateau\n";
+  cin >> taille;
+  switch(jeu){
+  case 1:
+    j= Jeu2048(taille);
+    break;
+  case 2:
+    j=JeuTaquin(taille);
+    break;
+  default:
+    return -1;
+  
+  }
+  j.play();
   
 }
